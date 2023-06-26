1e79c513a12d92d9f29070f0e4175987fadb8ad8
hushkmj <hushkmj@naver.com>
2023-06-27 00:01:02 +0900

    Transitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,13 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <button
-          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-400 hover:text-black active:bg-yellow-500 focus:text-red-500"
-        >
-          Checkout
-        </button>
+        <div className="flex justify-center">
+          <button
+            className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-400 hover:text-black active:bg-yellow-500 focus:text-red-500"
+          >
+            Checkout
+          </button>
+        </div>
       </div>
       {/* profile */}
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
@@ -62,9 +64,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-100" />
-              <button className="w-5 h-5 rounded-full bg-indigo-100" />
-              <button className="w-5 h-5 rounded-full bg-teal-100" />
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
             </div>
             <div className="flex items-center space-x-5">
               <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-</button>
