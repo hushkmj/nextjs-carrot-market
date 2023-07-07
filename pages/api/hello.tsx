42f7f972922aa54c6f7061dd518db3e7a4f167b0
hushkmj <hushkmj@naver.com>
2023-07-08 01:45:34 +0900

    Database setup

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import client from "../../libs/client"
 
 type Data = {
   name: string
@@ -9,5 +10,12 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  client.user.create({
+    data: {
+      email: "hushkmj@naver.com",
+      name: "zeroscrap",
+    }
+  })
+
   res.status(200).json({ name: 'John Doe' })
 }
