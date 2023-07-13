26579be6f3e16d22af7bad2143857615e912a33b
Moonjong Kim <hushkmj@naver.com>
2023-07-13 18:51:20 +0900

    withHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,18 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import client from "../../../libs/server/client"
+import withHandler from '@/libs/server/withHandler';
 
 type Data = {
   name: string
 }
 
-export default function handler(
+async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if(req.method !== "POST"){
-    res.status(401).end();
-  }
   console.log(req.body);
   res.status(200).end();
 }
+
+export default withHandler("POST", handler);
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -12,25 +12,13 @@ interface EnterForm {
 }
 const Enter: NextPage = () => {
     const [enter, {loading, data, error}] = useMutation("/api/users/enter");
-    const [submitting, setSubmitting] = useState(false);
     const { register, handleSubmit, reset } = useForm<EnterForm>();
     const [method, setMethod] = useState<"email" | "phone">("email");
     const onEmailClick = () => { reset(); setMethod("email") };
     const onPhoneClick = () => { reset(); setMethod("phone") };
     const onValid = (validForm: EnterForm) => {
-        // setSubmitting(true);
-        // fetch("/api/users/enter", {
-        //     method: "POST",
-        //     body: JSON.stringify(validForm),
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     }
-        // }).then(()=>{
-        //     setSubmitting(false);
-        // });
         enter(validForm);
     };
-    console.log(loading, data, error);
     return (
         <div className="mt-16 px-4">
             <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -89,7 +77,7 @@ const Enter: NextPage = () => {
                         <Button text={"Get login link"} />
                     ) : null}
                     {method === "phone" ? (
-                        <Button text={submitting ? "Loading" : "Get one-time password"} />
+                        <Button text={loading ? "Loading" : "Get one-time password"} />
                     ) : null}
                 </form>
                 <div className="mt-8">
