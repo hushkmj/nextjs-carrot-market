86bee2e28cae9c53315d0d6fe505a2c211b46f12
hushkmj <hushkmj@naver.com>
2023-07-15 00:51:57 +0900

    Twilio

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,18 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import client from "@/libs/server/client"
-import withHandler from '@/libs/server/withHandler';
 
-type Data = {
-  name: string
-}
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import client from "@/libs/server/client";
 
 async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: Number(phone) } : { email }
+  const user = phone ? { phone: Number(phone) } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -30,44 +28,8 @@ async function handler(
       }
     }
   });
-  console.log(token);
-  /*   if (email) {
-      console.log("found it")
-      user = await client.user.findUnique({
-        where: {
-          email,
-        },
-      });
-      if (!user) {
-        console.log("Did not find. Will create")
-        user = await client.user.create({
-          data: {
-            name: "Anonymous",
-            email,
-          }
-        })
-      }
-      console.log(user);
-    }
-    if (phone) {
-      console.log("found it")
-      user = await client.user.findUnique({
-        where: {
-          phone: +phone,
-        },
-      });
-      if (!user) {
-        console.log("Did not find. Will create")
-        user = await client.user.create({
-          data: {
-            name: "Anonymous",
-            phone: +phone,
-          }
-        })
-      }
-      console.log(user);
-    } */
-  res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
-
 export default withHandler("POST", handler);
